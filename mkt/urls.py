802c2f492faf5194970c42d0211f5398793da47b
Victor Hugo <victorblog410@gmail.com>
2021-06-08 16:43:06 -0300

    added marketplace crud

--- mkt/urls.py
+++ mkt/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='marketplace-list'),
-    path('create/mkt', views.new_mkt, name='mkt-add')
+    path('create/mkt', views.new_mkt, name='mkt-add'),
+    path('<int:pk>/delete/mkt', views.MktDeleteView.as_view(),
+         name='delete-mkt'),
+    path('mkt/update/<int:pk>', views.update_mkt, name='mkt-update'),
+
+
 
 ]
--- mkt/views.py
+++ mkt/views.py
@@ -1,10 +1,12 @@
 from django.views.generic import (
     CreateView,
     DeleteView,
-    DetailView,
     ListView,
     UpdateView
 )
+
+from django.urls import reverse_lazy as r
+ 
 from mkt.models import Marketplace
 
 
@@ -18,3 +20,14 @@ home = ListView.as_view(template_name='mkt_list.html',
 new_mkt = CreateView.as_view(template_name='add_mkt.html',
                              form_class=MarketplaceForm,
                              model=Marketplace)
+
+
+class MktDeleteView(DeleteView):
+    template_name = 'mkt_delete.html'
+    model = Marketplace
+    success_url = r('marketplace-list')
+
+
+update_mkt = UpdateView.as_view(template_name='update_mkt.html',
+                                model=Marketplace,
+                                form_class=MarketplaceForm)
